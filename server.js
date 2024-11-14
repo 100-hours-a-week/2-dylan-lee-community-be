d8c5096daef4ae6f8f5b6aa667bb4b2b92813b6e
Hongmin Lee <nimweaver14@gmail.com>
2024-11-14 23:33:03 +0900

    feat: 세션 설정 (#2)

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const session = require('express-session');
+const { DEFAULTS } = require('./constants');
 
 // const pagesRouter = require('./routes/pages');
 const userRoutes = require('./routes/userRoutes');
@@ -12,6 +14,19 @@ const PORT = process.env.PORT || 3000;
 
 // 미들웨어 설정
 app.use(express.json());
+app.use(
+    /* 세션 설정 */
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+            maxAge: DEFAULTS.SESSION_TIMEOUT,
+        },
+    })
+);
 
 // 라우터 등록
 app.use('/api/v1/users', userRoutes); // 사용자 관련 API
