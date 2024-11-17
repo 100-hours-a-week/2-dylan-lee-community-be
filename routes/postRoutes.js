adb0daec82d3c7c086f5c61b994c27325b2b2645
Hongmin Lee <nimweaver14@gmail.com>
2024-11-18 00:07:13 +0900

    feat: makePost API 구현 (#14)

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,6 +6,9 @@ const router = express.Router();
 // 페이지네이션된 포스트 목록 조회 라우트
 router.get('/', postController.getPaginatedPosts);
 
+// 포스트 생성 라우트
+router.post('/', postController.createPost);
+
 // ID로 단일 포스트 조회 라우트
 router.get('/:id', postController.getPostById);
 
